ca9c96629ab4c54fe6143fa6f7751d3a0ea71cd9
xiaole fang <xiaole.fang@deepmirror.com>
2022-06-13 11:38:05 +0800

    update

--- apps/tiger/docker/tiger-web.dockerfile
+++
@@ -1,6 +0,0 @@
-FROM nginx:latest
-
-# Add the execution file
-ADD dist /html
-ADD certs /certs
-ADD nginx_default.conf /etc/nginx/conf.d/default.conf
--- apps/tiger/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
-import App from './App.js';
-
-ReactDOM.render(<App />, document.getElementById('root'));
